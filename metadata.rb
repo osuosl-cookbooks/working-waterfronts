665a129b704d466fa4ad411a01165a39c5c7a87c
Evan Tschuy <tschuy@osuosl.org>
2014-09-16 12:02:38 -0700

    Bump version to v0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      "Installs/Configures What's Fresh dependencies"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'application'
 depends          'application_python'
